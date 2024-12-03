1f7fe1ddd6f4234f024fa54d92bfef3809f53095
mrmabuzakk <mrmabuzakk@gmail.com>
2024-12-03 09:51:59 +0200

    Update footer with personal GitHub link

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>
-        {new Date().getFullYear()} <a href="https://headsup.co" target="_blank" rel="noopener noreferrer">headsup</a>. All rights reserved. | Developed by Kulani Mabuza
+        {new Date().getFullYear()} <a href="https://headsup.co" target="_blank" rel="noopener noreferrer">HeadsUp</a>. All rights reserved. Developed by <a href="https://github.com/MrMabuzaKK" target="_blank" rel="noopener noreferrer">Kulani Mabuza</a>
       </p>
     </footer>
   );
